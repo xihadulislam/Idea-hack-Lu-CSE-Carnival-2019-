f9c44b53fd6bf8a7f59c667ba7e777c0780b1334
xihad <xihad.bd@gmail.com>
2019-10-19 12:03:41 +0600

    init commit

---
+++ app/src/main/java/teamx/ideahacks/SignupActivity.java
@@ -0,0 +1,15 @@
+package teamx.ideahacks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
+
